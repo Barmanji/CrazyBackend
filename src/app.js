4beb3bea824d0a8815044c91bf33c25a06ca7876
Ajay Barman <barmanjiaj@gmail.com>
2024-12-15 19:30:05 +0530

    feat: added util

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ app.use(cors({
  credentials: true
 }))
 
-const app = express ();
+app.use(express.json({limit: '16kb'}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
 
 export {app}
