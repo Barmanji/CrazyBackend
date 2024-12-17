c6861a943584fc67205d6b0f1cc435d4f17b3523
Ajay Barman <barmanjiaj@gmail.com>
2024-12-17 12:21:45 +0530

    feat: utils

---
+++ src/utils/ApiError.js
@@ -0,0 +1,25 @@
+class ApiError extends Error{
+    constructor(
+        statusCode,
+        message = "something went wrong",
+        errors = [],
+        stack = ""
+    ){
+        //while overwriting constructor add super!! CLASSES 101
+        super(message)
+        this.statusCode = statusCode
+        this.data = null
+        this.message = message
+        this.success = false;
+        this.errors = this.errors
+
+        //best prod grade things
+        if(stack) {
+            this.stack = stack
+        } else {
+            Error.captureStackTrace(this, this.constructor)
+        }
+    }
+}
+
+export {ApiError}
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,30 @@
-const asyncHandler = () => {
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next))
+            .catch((err)=>next(err))
+    }
 
 }
 
 export {asyncHandler};
+//-------------------learnings---------------------
+//cont higherOrFunc = (func) => async() => {
+// abc+xyz;
+// return} just function me function then fucntion will be treated as a variable
+//
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async () => {}
+
+
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({    .josn file aayega toh usko bhi handle kra lo
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
+
