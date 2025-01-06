33bba8856429c502f6c63829385b86622da99d6d
Ajay Barman <barmanjiaj@gmail.com>
2025-01-06 21:38:47 +0530

    feat : updated controller, user.route, ApiError

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,21 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
+
 
 const router = Router()
 
-
-router.route("/register").post(registerUser) //confusing Syntax, ====READ ABOUT IT====
+router.route("/register").post(
+    //injecting middleware!! for file handling
+    upload.fields([
+        {
+            name: "avatar", maxCount: 1
+        },
+        {
+            name: "coverImage", maxCount: 1
+        }
+    ]),
+    registerUser
+) //confusing Syntax, ====READ ABOUT IT====
 
 export default router;
