4f0766cb863a0dd9c3a64621d4041152b5f60e72
Ajay Barman <barmanjiaj@gmail.com>
2025-01-12 19:32:37 +0530

    feat: update middleware, added auth.middleware.js

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,5 +1,20 @@
+import { ApiError } from "../utils/ApiError";
 import { asyncHandler } from "../utils/asyncHandler";
+import jwt from "jsonwebtoken"
+import { User } from "../models/user.model";
+const VerifyExistenceOfUserByJWT = asyncHandler( async(req, res, next) => {
+    //cookies?. is becuase movile donest store cookies thats why do that always
+    const token = await req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "") //this gives me access of all cookies as i have acess it cia cookie parser ini index js, bearer ko replace kro empty str
+})
 
-const VerifyExistenceOfUserByJWT = asyncHandler
+if(!token) {
+    throw new ApiError(401, "Unautharized Request")
+}
 
+const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
+const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
+
+if (!user) {
+    throw new ApiError(401, "Invalid Access Token")
+}
 export default existenceOfUser;
