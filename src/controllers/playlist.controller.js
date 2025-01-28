b5a6053a885bb6b97ceb97121ac405f5e3c9a380
Ajay Barman <barmanjiaj@gmail.com>
2025-01-28 20:04:16 +0530

    playlist contoller: update deletion of both video inside playllist and plalysits is done

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -82,20 +82,82 @@ const addVideoToPlaylist = asyncHandler(async (req, res) => {
 })
 
 const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
-    const {playlistId, videoId} = req.params
-    // TODO: remove video from playlist
+    const {playlistId, videoId} = req.params;
+    if([playlistId, videoId].some(field => !field.trim())){
+        throw new ApiError(400, "Playlist and Video are must")
+    }
+    if (!isValidObjectId(playlistId) || !isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid Playlist or Video ID");
+    }
+    const playlist = await Playlist.findById(playlistId);
+    if (!playlist) {
+        throw new ApiError(400, "No Playlist found");
+    }
+    if (playlist.owner.toString() !== req.user._id.toString()) {
+        throw new ApiError(403, "You are not allowed to modify this playlist");
+    }
+    const videoExists = playlist.video.filter(
+        (video) => video.toString() === videoId
+    );
+    if (videoExists.length < 0) {
+        throw new ApiError(400, "Video doesn't exist in playlist");
+    }
 
+    await Playlist.findByIdAndUpdate(
+        playlistId,
+        { $pull: { video: videoId } }, // pull deletes the videoId in video (all occurence) Example: If playlist.video contains [vid1, vid2, vid3] and videoId = vid2, $pull will update it to [vid1, vid3].
+        { new: true } // Return the updated document
+    );
+
+    return res.status(200)
+    .json(new ApiResponse(200, {}, "video has been deleted successfully"))
 })
 
 const deletePlaylist = asyncHandler(async (req, res) => {
     const {playlistId} = req.params
     // TODO: delete playlist
+    if(!playlistId || !isValidObjectId(playlistId)){
+        throw new ApiError(404, "plalist is invalid")
+    }
+    const playlist = await Playlist.findById(playlistId)
+    if (playlist.owner.toString() !== req.user._id.toString()) {
+        throw new ApiError(403, "You are not allowed to modify this playlist");
+    }
+    await Playlist.findByIdAndDelete(playlistId)
+    return res.status(200)
+    .json(new ApiResponse(200, null, "playlist is successfully deleted"))
 })
 
 const updatePlaylist = asyncHandler(async (req, res) => {
     const {playlistId} = req.params
     const {name, description} = req.body
     //TODO: update playlist
+    if(!playlistId || !isValidObjectId(playlistId)){
+        throw new ApiError(400, "playlist is invalid")
+    }
+    //if([name, description].some(field => !field.trim())){
+    //    throw new ApiError(400, "Name and description are required")
+    //}
+    const playlist = await Playlist.findById(playlistId)
+    if (playlist.owner.toString() !== req.user._id.toString()) {
+        throw new ApiError(403, "You are not allowed to modify this playlist");
+    }
+
+    const oldName = playlist.name;
+    const oldDescription = playlist.description;
+    const updatePlaylist = await Playlist.findByIdAndUpdate(
+        playlistId,
+        {
+            $set: {
+                name: name || oldName,
+                description: description || oldDescription
+            }
+        },
+        {new: true}
+    )
+
+    return res.status(200)
+    .json(new ApiResponse(200, updatePlaylist, "Playlist is updated successfully"))
 })
 
 export {
